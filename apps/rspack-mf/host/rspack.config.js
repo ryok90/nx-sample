adb633b34ad24852f9d45afc56de36827467fdd2
Rodrigo Yokota <ryok90@gmail.com>
2024-07-31 22:35:26 -0300

    replicate error with @rspack/core mf

--- apps/rspack-mf/host/rspack.config.js
+++ apps/rspack-mf/host/rspack.config.js
@@ -1,9 +1,9 @@
 const { composePlugins, withNx, withReact } = require('@nx/rspack');
 const path = require('path');
-// const { ModuleFederationPlugin } = require('@rspack/core').container;
-const {
-  ModuleFederationPlugin,
-} = require('@module-federation/enhanced/rspack');
+const { ModuleFederationPlugin } = require('@rspack/core').container;
+// const {
+//   ModuleFederationPlugin,
+// } = require('@module-federation/enhanced/rspack');
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   config.output.publicPath = 'auto';
